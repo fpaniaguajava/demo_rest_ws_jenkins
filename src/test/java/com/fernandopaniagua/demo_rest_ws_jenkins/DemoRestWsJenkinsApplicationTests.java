5ccb89c89736f19777a2440b258ce4c9a6e0e79d
lafabricadeandroides <fernando.paniagua.java@gmail.com>
2025-05-12 11:33:04 +0200

    Update DemoRestWsJenkinsApplicationTests.java

--- src/test/java/com/fernandopaniagua/demo_rest_ws_jenkins/DemoRestWsJenkinsApplicationTests.java
+++ src/test/java/com/fernandopaniagua/demo_rest_ws_jenkins/DemoRestWsJenkinsApplicationTests.java
@@ -2,12 +2,15 @@ package com.fernandopaniagua.demo_rest_ws_jenkins;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoRestWsJenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int resultado = 2 + 2;
+		assertEquals(5, resultado, "La suma de 2 + 2 debería ser 4");
 	}
 
 }
